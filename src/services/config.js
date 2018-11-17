b41ea8d88a58efffbbd8a592d0771d8ac9d21d16
José Pinto <sleekfox123@gmail.com>
2018-11-17 11:46:21 +0000

    added .env | updated package.json app release version

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 const api = {
-  username: "admin",
-  password: "district",
+  username: process.env.REACT_APP_API_USERNAME,
+  password: process.env.REACT_APP_API_PASSWORD,
   base: "https://play.dhis2.org/2.30/api/",
   users: {
     url: queryString => getApiUrl(`users.json${queryString}`)
